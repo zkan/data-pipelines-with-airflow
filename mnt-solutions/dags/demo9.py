f203bbef5faeb64943ede3d4830745d6537c7b6f
zkan <zkan.cs@gmail.com>
2022-02-16 16:25:56 +0700

    Add sensor demo

---
+++ mnt-solutions/dags/demo9.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.sensors.filesystem import FileSensor
+from airflow.utils import timezone
+
+
+default_args = {
+    "owner": "zkan",
+    "start_date": timezone.datetime(2022, 2, 1),
+}
+with DAG(
+    "demo9",
+    default_args=default_args,
+    schedule_interval="@daily",
+) as dag:
+
+    is_file_available = FileSensor(
+        task_id="is_file_available",
+        fs_conn_id="fs",
+        filepath="/opt/airflow/dags/hello.txt",
+        poke_interval=5,
+        timeout=100,
+    )
+
