9370c0e6987ec253807812ebf4ea61bc4bf3ed6f
zkan <zkan.cs@gmail.com>
2022-02-16 15:38:06 +0700

    Add hook (+connection) demo

---
+++ mnt-solutions/dags/demo8.py
@@ -0,0 +1,39 @@
+import logging
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils import timezone
+
+
+def _query_data():
+    pg_hook = PostgresHook(
+        postgres_conn_id="airflow_metastore",
+        schema="airflow",
+    )
+    connection = pg_hook.get_conn()
+    cursor = connection.cursor()
+
+    sql = """
+        SELECT dag_id, owners FROM dag
+    """
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    for each in rows:
+        print(each)
+
+
+default_args = {
+    "owner": "zkan",
+    "start_date": timezone.datetime(2022, 2, 1),
+}
+with DAG(
+    "demo8",
+    default_args=default_args,
+    schedule_interval="@daily",
+) as dag:
+
+    query_data = PythonOperator(
+        task_id="query_data",
+        python_callable=_query_data,
+    )
